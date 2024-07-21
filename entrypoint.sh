a9c15670df4604faccf292f8124c8c270ff26cfb
propan <ldspdk5v@gmail.com>
2024-07-21 13:44:50 +0900

    Added CD v1

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,7 +7,8 @@ rm -f /api/tmp/pids/server.pid
 if [ "$RAILS_ENV" = "production" ]; then
   # bundle exec rails db:create
   # bundle exec rails db:migrate
-  bundle exec rails db:seed
+  # bundle exec rails db:seed
+  # bundle exec rails db:reset
 fi
 
 exec "$@"
